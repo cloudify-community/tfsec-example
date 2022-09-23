4bba39c7c96e9f06469f0ac03662325b7eced074
acritelli <anthony@cloudify.co>
2022-09-23 15:45:03 -0400

    Remove tfsec ignore

--- tf_module/ec2-instance/main.tf
+++ tf_module/ec2-instance/main.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "example_security_group" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # tfsec:ignore:aws-ec2-no-public-egress-sgr
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
